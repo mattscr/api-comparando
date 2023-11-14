0374b5382ee73b1f1e2db157874d12698ab62350
Matt <matias.rodriguez.e@gmail.com>
2023-11-13 21:22:37 -0300

    controller para get by id, update y delete

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -11,11 +11,54 @@ export const controllerCreateProduct = async (req, res) => {
   }
 };
 
-//controller listar productos
+//controller list all products
 export const controllerListProduct = async (req, res) => {
   try {
     const allProducts = await ProductModel.find({}, "-__v");
     res.json(allProducts);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+//controller get product by id
+export const controllerGetProductId = async (req, res) => {
+  try {
+    const { producto_id } = req.params;
+    const product = await ProductModel.findOne({ id: producto_id });
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+//controller for update
+export const controllerUpdateProduct = async (req, res) => {
+  try {
+    const { producto_id } = req.params;
+    const updatedProduct = await ProductModel.findOneAndUpdate(
+      { id: producto_id },
+      req.body,
+      { new: true }
+    );
+
+    res.json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+//delete product
+export const controllerDeleteProduct = async (req, res) => {
+  try {
+    const { producto_id } = req.params;
+    const deletedProduct = await ProductModel.findOneAndDelete({
+      id: producto_id,
+    });
+    res.json(deletedProduct);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,24 +2,22 @@ import { Router } from "express";
 import {
   controllerCreateProduct,
   controllerListProduct,
+  controllerGetProductId,
+  controllerUpdateProduct,
+  controllerDeleteProduct,
 } from "../controllers/product.controller.js";
 
 const productRouter = Router();
+
 //get all of products
 productRouter.get("/", controllerListProduct, (req, res) => {
   res.send("<h2>productos!</h2>");
 });
-//get of categories
-productRouter.get("/categories", (req, res) => {
-  res.send("<h2>categorias!</h2>");
-});
-//get product of a category
-productRouter.get("/categories/:categories");
 //get product by id
-productRouter.get("/:id");
-
+productRouter.get("/:id", controllerGetProductId);
+//create product
 productRouter.post("/", controllerCreateProduct);
-productRouter.put("/:id");
-productRouter.delete("/:id");
+productRouter.put("/:id", controllerUpdateProduct);
+productRouter.delete("/:id", controllerDeleteProduct);
 
 export default productRouter;
